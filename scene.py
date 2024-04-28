8c438b9a2ab9387289172c9a79becf7c09629391
tobias17 <tobiasfischer17@gmail.com>
2024-04-27 21:18:48 -0400

    appearing test

--- scene.py
+++ scene.py
@@ -1,6 +1,6 @@
-from manim import Scene, Square, VGroup, ManimColor # type: ignore
+from manim import Scene, Square, VGroup, ManimColor, FadeIn # type: ignore
 import functools, operator
-from typing import List, Tuple, Iterable, Union, TypeVar, Optional, Iterator
+from typing import List, Tuple, Iterable, Union, TypeVar
 
 DEFAULT_VIEW_HEIGHT = 3.0
 DEFAULT_VIEW_WIDTH  = 6.0
@@ -13,7 +13,7 @@ T = TypeVar("T")
 def prod(x:Iterable[T]) -> Union[T,int]: return functools.reduce(operator.mul, x, 1)
 
 class TensorView:
-    def __init__(self, *shape, color:Tuple[int,int,int]=BLUE, min_c:float=0.1, max_c:float=0.9):
+    def __init__(self, *shape, color:Tuple[int,int,int]=BLUE, min_c:float=0.1, max_c:float=0.9, opacity:float=1.0):
         self.shape = shape
         def lerp_color(n, N) -> ManimColor:
             def l(v): return int(v * (min_c + (n/N * (max_c - min_c))))
@@ -21,7 +21,7 @@ class TensorView:
         assert len(shape) > 0 and len(shape) < 3, f"Can only view 1D and 2D shapes, found {len(shape)}D shape"
         if len(shape) == 1: shape = (shape[0],1)
         w, h = self.shape[0], (self.shape[1] if len(self.shape) > 1 else 1)
-        self.grid = VGroup(VGroup(*[Square(fill_color=lerp_color(i, prod(shape)), fill_opacity=1) for i in range(prod(shape))]))
+        self.grid = VGroup(VGroup(*[Square(fill_color=lerp_color(i, prod(shape)), fill_opacity=opacity) for i in range(prod(shape))]))
         for x in range(w):
             for y in range(h):
                 i = x + y * w
@@ -39,8 +39,11 @@ class CreateGrid(Scene):
         tv_2.grid.move_to([ 3.5, 2.0, 0]).scale(5)
         self.add(tv_2.grid)
 
-        tv_3 = TensorView(10, 5, color=GREEN)
+        tv_3 = TensorView(10, 5, color=GREEN, opacity=0.0)
         tv_3.grid.move_to([0, -2.0, 0]).scale(5)
         self.add(tv_3.grid)
 
+        self.wait()
+        for i in range(10*5):
+            self.play(tv_3.grid[0][i].animate.set_fill(opacity=1.0), run_time=0.1)
         self.wait()
