709c6b1b92b9b960123046f3594c83e0186cee7c
tobias17 <tobiasfischer17@gmail.com>
2024-04-27 21:46:16 -0400

    full animation

--- scene.py
+++ scene.py
@@ -1,4 +1,4 @@
-from manim import Scene, Square, VGroup, ManimColor, FadeIn # type: ignore
+from manim import Scene, Square, VGroup, ManimColor, Animation, LEFT # type: ignore
 import functools, operator
 from typing import List, Tuple, Iterable, Union, TypeVar
 
@@ -21,12 +21,18 @@ class TensorView:
         assert len(shape) > 0 and len(shape) < 3, f"Can only view 1D and 2D shapes, found {len(shape)}D shape"
         if len(shape) == 1: shape = (shape[0],1)
         w, h = self.shape[0], (self.shape[1] if len(self.shape) > 1 else 1)
-        self.grid = VGroup(VGroup(*[Square(fill_color=lerp_color(i, prod(shape)), fill_opacity=opacity) for i in range(prod(shape))]))
+        self.grid = VGroup(VGroup(*[Square(fill_color=lerp_color(i, prod(shape)), fill_opacity=opacity) for i in range(prod(shape))], Square(stroke_opacity=0.0).scale(0.7)))
         for x in range(w):
             for y in range(h):
                 i = x + y * w
-                self.grid[0][i].move_to([(-w/2.0) + x + 0.5, (h/2.0) - y - 0.5, 0]).scale(0.5)
-        self.grid[0].scale(1 / max(w, h))
+                self.grid[0][i].move_to([x, y, 0]).scale(0.5)
+        self.grid[0].move_to([-w, -h, 0]).scale(1 / max(w, h)).flip(LEFT)
+    
+    def indicator_opacity_anim(self, opacity:float) -> Animation:
+        return self.grid[0][-1].animate.set_stroke(opacity=opacity)
+
+    def indicator_move_anim(self, index:int) -> Animation:
+        return self.grid[0][-1].animate.move_to(self.grid[0][index].get_center())
 
 class CreateGrid(Scene):
     def construct(self):
@@ -43,7 +49,10 @@ class CreateGrid(Scene):
         tv_3.grid.move_to([0, -2.0, 0]).scale(5)
         self.add(tv_3.grid)
 
-        self.wait()
-        for i in range(10*5):
+        self.play(tv_1.indicator_opacity_anim(1.0), tv_2.indicator_opacity_anim(1.0), tv_3.indicator_opacity_anim(1.0), run_time=0.5)
+        self.play(tv_3.grid[0][0].animate.set_fill(opacity=1.0), run_time=0.1)
+        for i in range(1, 10*5):
+            self.play(*[tv.indicator_move_anim(i) for tv in [tv_1, tv_2, tv_3]], run_time=0.1)
             self.play(tv_3.grid[0][i].animate.set_fill(opacity=1.0), run_time=0.1)
+        self.play(tv_1.indicator_opacity_anim(0.0), tv_2.indicator_opacity_anim(0.0), tv_3.indicator_opacity_anim(0.0), run_time=0.5)
         self.wait()
