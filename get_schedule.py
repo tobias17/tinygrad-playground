62e0f52ef531a4ea51379daff23c5ec722d7f98e
tobias17 <tobiasfischer17@gmail.com>
2024-04-30 18:00:45 -0400

    more inspection

--- get_schedule.py
+++ get_schedule.py
@@ -32,27 +32,31 @@ def get_contents(buf:Buffer) -> np.ndarray:
 def parse_schedule(op:LazyOp, bufs:List[Buffer], depth:int=0):
     text = f"{op.op}"
     if op.op == BufferOps.LOAD:
-        text += f" <shape {op.arg.st.shape}> <strided {tuple(a*b for a,b in zip(op.arg.st.shape, op.arg.st.real_strides()))}> <idx {op.arg.idx}>"
         print(get_contents(bufs[op.arg.idx]))
+        if any(v is None for v in op.arg.st.real_strides()):
+            print("  "*depth + f"Before: {op.arg.st.real_strides()}")
+            op.arg.st.simplify()
+            print("  "*depth + f"After:  {op.arg.st.real_strides()}")
+        text += f" <shape {op.arg.st.shape}> <strides {op.arg.st.real_strides()}> <idx {op.arg.idx}> <exp {op.arg.st.expr_idxs()}>"
     print("  "*depth + text)
     for src in op.src:
         parse_schedule(src, bufs, depth+1)
 
 if __name__ == "__main__":
-    a = Tensor.arange(10).reshape(1,10)#.realize()
-    b = Tensor.arange(5).reshape(5,1)  #.realize()
+    a = Tensor.arange(10).reshape(1,10).realize()
+    b = Tensor.arange(5).reshape(5,1).expand(5,2).reshape(1,10).realize()
 
     c = a + b
 
-    DO_VIZ = True
-    c.realize()
+    # DO_VIZ = True
+    # c.realize()
 
-    # schedule = c.schedule_with_vars()
-    # for ei in schedule:
-    #     for si in ei:
-    #         print(si)
-    #         print("Buffers:")
-    #         for buf in si.bufs:
-    #             print("  " + str(buf))
-    #         for ast in si.ast:
-    #             parse_schedule(ast, si.bufs)
+    schedule = c.schedule_with_vars()
+    for ei in schedule:
+        for si in ei:
+            # print(si)
+            print("Buffers:")
+            for buf in si.bufs:
+                print("  " + str(buf))
+            for ast in si.ast:
+                parse_schedule(ast, si.bufs)
