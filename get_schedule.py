62b6bce3fa10abe86f9c6bb19f74a34df005abc7
tobias17 <tobiasfischer17@gmail.com>
2024-04-30 22:29:25 -0400

    printing entire compute tree

--- get_schedule.py
+++ get_schedule.py
@@ -1,8 +1,6 @@
 import sys, os
 import numpy as np # type: ignore
-from typing import List
-
-DO_VIZ = False
+from typing import List, Set
 
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "tinygrad"))
@@ -11,18 +9,21 @@ from tinygrad.ops import LazyOp, BufferOps, LoadOps # type: ignore
 from tinygrad.buffer import Buffer # type: ignore
 from tinygrad.lazy import LazyBuffer # type: ignore
 from tinygrad.features.graph import Inject # type: ignore
+
+DO_VIZ = False
+seen: Set[LazyBuffer] = set()
 def visual_lazybuffer(lb:'LazyBuffer', scheduled=False, depth=0):
-    global DO_VIZ
+    global DO_VIZ, seen
     if DO_VIZ:
-        if lb.base.realized is None and lb.base.op is LoadOps.CONST: return
-        if lb.base != lb:
-            lb = lb.base
-        if lb.realized is None:
-            print("  "*depth + str(lb))
-            for x in lb.srcs:
-                visual_lazybuffer(x, scheduled, depth+1)
-        else:
-            print("  "*depth + f"(R) {lb}")
+        if lb in seen:
+            return
+        seen.add(lb)
+        print("  "*depth + str(lb))
+        if hasattr(lb, "srcs"):
+            for src in lb.srcs:
+                visual_lazybuffer(src, depth=depth+1)
+        elif lb != lb.base:
+            visual_lazybuffer(lb.base, depth=depth+1)
 Inject.visual_lazybuffer = visual_lazybuffer
 
 
@@ -43,20 +44,20 @@ def parse_schedule(op:LazyOp, bufs:List[Buffer], depth:int=0):
         parse_schedule(src, bufs, depth+1)
 
 if __name__ == "__main__":
-    a = Tensor.arange(10).reshape(1,10).realize()
-    b = Tensor.arange(5).reshape(5,1).expand(5,2).reshape(1,10).realize()
+    a = Tensor.arange(10).reshape(1,10)
+    b = Tensor.arange(5).reshape(5,1).expand(5,2).reshape(1,10)
 
     c = a + b
 
-    # DO_VIZ = True
-    # c.realize()
+    DO_VIZ = True
+    c.realize()
 
-    schedule = c.schedule_with_vars()
-    for ei in schedule:
-        for si in ei:
-            # print(si)
-            print("Buffers:")
-            for buf in si.bufs:
-                print("  " + str(buf))
-            for ast in si.ast:
-                parse_schedule(ast, si.bufs)
+    # schedule = c.schedule_with_vars()
+    # for ei in schedule:
+    #     for si in ei:
+    #         # print(si)
+    #         print("Buffers:")
+    #         for buf in si.bufs:
+    #             print("  " + str(buf))
+    #         for ast in si.ast:
+    #             parse_schedule(ast, si.bufs)
